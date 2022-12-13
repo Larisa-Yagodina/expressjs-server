e2c31d97970bbb6736020ba7f321476c9a5c0770
larisayagodina <cheeseroll@mail.ru>
2022-12-13 17:27:57 +0300

    add home

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -1,7 +1,9 @@
 import tasksRouter from '../tasks/TasksRoutes';
 import statusesRouter from '../statuses/StatusesRoutes';
+import home from '../home';
 
 export default function routes(app){
+  app.use('/', home)
   app.use('/tasks', tasksRouter)
   app.use('/statuses', statusesRouter)
 }
