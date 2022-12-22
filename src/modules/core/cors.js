7f0f36ebf59ef5129c58d8836c238d53814a90c7
larisayagodina <cheeseroll@mail.ru>
2022-12-22 18:45:54 +0300

    cors bug fixed

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -2,7 +2,7 @@ export default function cors (app){
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-      'Access-Control_Allow-Headers',
+      'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     );
     if (req.method === "OPTIONS"){
