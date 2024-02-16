7f80a2b5af8e24f5d4df4ef139c422d9f420bb9b
larisayagodina <cheeseroll@mail.ru>
2024-02-16 16:02:47 +0300

    fixed errors

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -6,6 +6,7 @@ async function dbConnect() {
   await mongoose.connect('mongodb+srv://admin:backloop@cluster1.xyeasnb.mongodb.net/kanban');
 }
 
+//mongodb+srv://admin:<password>@cluster0.eq08xyg.mongodb.net/
 dbConnect().then(res => console.log('CONNECTED'));
 dbConnect().catch(err => console.log(err));
 
