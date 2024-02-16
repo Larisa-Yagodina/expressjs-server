6cbf13b1f8e3e998cc35b8f453499d6dd309d521
larisayagodina <cheeseroll@mail.ru>
2024-02-16 14:02:33 +0300

    Revert "Initial commit"
    This reverts commit 465ed25c

--- src/modules/core/logger.js
+++
@@ -1,5 +0,0 @@
-import morgan from 'morgan';
-
-export default function logger(app){
-  app.use(morgan('dev'));
-}
--- src/modules/core/parseResponse.js
+++
@@ -1,6 +0,0 @@
-import bodyParser from 'body-parser'
-
-export default function parseResponse(app){
-  app.use(bodyParser.urlencoded({extended: false}))
-  app.use(bodyParser.json())
-}
