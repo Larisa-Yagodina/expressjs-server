00aec0af7e748293f7a783df356eb4d2ebee7e1c
larisayagodina <cheeseroll@mail.ru>
2025-02-06 20:52:50 +0300

    fixed home

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,3 +1,5 @@
 export default function home(req, res) {
-  res.status(202).json('It`s a home page! Also try /tasks, /statuses, /clients, /orders, /services or /todolist');
+  // res.status(202).json('It`s a home page! Also try /tasks, /statuses, /clients, /orders, /services or /todolist');
+  res.status(202).json('It`s a home page! Also try /tasks, /statuses or /todolist');
+
 }
